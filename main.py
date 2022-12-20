a2549dbbb3569b040cc1027c170273993f4e5d14
Akshat Girdhar <84849106+coderakg@users.noreply.github.com>
2022-12-20 16:52:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -49,18 +49,18 @@ window.config(padx=50,pady=50,bg=BACKGROUND_COLOR)
 flip_timer = window.after(4000,func=flip_card)  
 
 canvas = Canvas(width=800,height=526,bg=BACKGROUND_COLOR,highlightthickness=0)
-card_front_img = PhotoImage(file="images\card_front.png")
-card_back_img = PhotoImage(file="images\card_back.png")
+card_front_img = PhotoImage(file="card_front.png")
+card_back_img = PhotoImage(file="card_back.png")
 card_bg = canvas.create_image(400,263,image = card_front_img)
 card_title = canvas.create_text(400,150,text=f"title",font=TITLE_FONT)
 de_word = canvas.create_text(400,263,text="word",font=WORD_FONT)
 canvas.grid(column=0,row=0,columnspan=2)
 
-cross_img = PhotoImage(file="images\wrong.png")
+cross_img = PhotoImage(file="wrong.png")
 cross_button = Button(image=cross_img,highlightthickness=0,command=next_card)
 cross_button.grid(row=1,column=0)
 
-right_img = PhotoImage(file="images/right.png")
+right_img = PhotoImage(file="right.png")
 right_button = Button(image=right_img,highlightthickness=0,command=known_card)
 right_button.grid(row=1,column=1)
 
